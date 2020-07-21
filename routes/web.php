a2b321d83f32ec60b25932b2aad7bca8cb5add09
hanjiaxuan <847869636@qq.com>
2020-07-21 21:47:22 +0800

    购物车测试版

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/cartlist/{id}","Index\CartController@cartlist");
+Route::get("/cart/gopay","Index\CartController@gopay");
